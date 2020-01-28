3a8057545e9a792b0d53bc1cb9d2d553e26b7732
Torben Carstens <github@carstens.tech>
2020-01-28 22:21:37 +0100

    Add new graphql endpoints

--- src/graphql/schema.rs
+++ src/graphql/schema.rs
@@ -1,15 +1,58 @@
 use juniper::FieldResult;
 
-use crate::database::AccessoryCategory;
+use crate::database::{self, AccessoryCategory, CocktailCategory, Glass, IngredientCategory};
 use crate::graphql::{Constraints, Context, MutationRoot, QueryRoot};
+use crate::models::{Accessory, Cocktail, CocktailAccessory, CocktailIngredient, Ingredient};
 
 #[juniper::object (Context = Context)]
 impl MutationRoot {}
 
 #[juniper::object (Context = Context)]
 impl QueryRoot {
+    fn accessories(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<Accessory>> {
+        database::Accessory::get(
+            constraints.unwrap_or_default().into(),
+            &context.connection.0,
+        ).map_err(Into::into)
+    }
+
     fn accessory_categories(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<AccessoryCategory>> {
         AccessoryCategory::get(
+            constraints.unwrap_or_default().into(),
+            &context.connection.0,
+        ).map_err(Into::into)
+    }
+
+    fn cocktails(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<Cocktail>> {
+        database::Cocktail::get(
+            constraints.unwrap_or_default().into(),
+            &context.connection.0,
+        ).map_err(Into::into)
+    }
+
+    fn cocktail_categories(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<CocktailCategory>> {
+        database::CocktailCategory::get(
+            constraints.unwrap_or_default().into(),
+            &context.connection.0,
+        ).map_err(Into::into)
+    }
+
+    fn glasses(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<Glass>> {
+        Glass::get(
+            constraints.unwrap_or_default().into(),
+            &context.connection.0,
+        ).map_err(Into::into)
+    }
+
+    fn ingredients(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<Ingredient>> {
+        database::Ingredient::get(
+            constraints.unwrap_or_default().into(),
+            &context.connection.0,
+        ).map_err(Into::into)
+    }
+
+    fn ingredient_categories(&self, context: &Context, constraints: Option<Constraints>) -> FieldResult<Vec<IngredientCategory>> {
+        IngredientCategory::get(
             constraints.unwrap_or_default().into(),
             &context.connection.0,
         ).map_err(Into::into)
