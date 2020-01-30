bd09bf3c6a1ee80ff37fcded7237910516762688
Torben Carstens <github@carstens.tech>
2020-01-30 15:38:36 +0100

    Add AccessoryInputQuery

---
+++ src/graphql/queries.rs
@@ -0,0 +1,7 @@
+#[derive(Debug, GraphQLObject, Queryable)]
+pub struct AccessoryCategoryQuery {
+    pub id: Option<i32>,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub image_link: Option<String>,
+}
